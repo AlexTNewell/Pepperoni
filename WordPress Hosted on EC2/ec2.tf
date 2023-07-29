538814db234c81406f726e36be23ffd3183e1998
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-07-29 18:41:07 +0100

    Update ec2.tf

--- WordPress Hosted on EC2/ec2.tf
+++ WordPress Hosted on EC2/ec2.tf
@@ -23,22 +23,6 @@ resource "local_file" "pepperoni_tf_key" {
 
 ##################### Setup Server EC2 Instance #####################
 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
-}
-
 resource "aws_instance" "setup_server" {
   ami           = data.aws_ami.amazon_linux_2.id  # Correctly reference the Amazon Linux 2 AMI data source
   instance_type = "t2.micro"
