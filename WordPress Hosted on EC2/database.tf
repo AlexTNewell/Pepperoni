ebd2ae39f312d943e51bfe4c5238e87b8a0566e2
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-07-29 19:10:44 +0100

    Add files via upload

---
+++ WordPress Hosted on EC2/database.tf
@@ -0,0 +1,26 @@
+##################### RDS #####################
+
+resource "aws_db_instance" "rds_db" {
+  allocated_storage    = 10
+  db_name              = "rds_db"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  identifier           = "dev_rds_db"
+  instance_class       = "db.t2.micro"
+  username             = "beautiful_coalscuttle"
+  password             = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids = aws_security_group.db_sg.id
+  availability_zone = var.secondary_region
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+##################### Database Subnet Groups #####################
+
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "db_subnet"
+  description = "Database Subnet"
+  subnet_ids = [aws_subnet.pri_db_az_1.id, aws_subnet.pri_db_az_2.id]
+
+}
---
+++ WordPress Hosted on EC2/security.tf
@@ -0,0 +1,148 @@
+
+##################### ALB Security Group #####################
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_sg"
+  description = "ALB Security Group"
+  vpc_id      = aws_vpc.dev_vpc.id
+}
+
+resource "aws_security_group_rule" "alb_http_sg_inbound_rule_1" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_security_group_rule" "alb_https_sg_inbound_rule_2" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
+
+
+##################### SSH Security Group #####################
+
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh_sg"
+  description = "SSH Security Group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+}
+
+resource "aws_security_group_rule" "ssh_sg_inbound_rule_1" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [data.http.my_public_ip.body]
+  security_group_id = aws_security_group.ssh_sg.id
+}
+
+##################### Web Server Security Group #####################
+
+resource "aws_security_group" "webserver_sg" {
+  name        = "webserver_sg"
+  description = "Webserver Security Group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+}
+
+resource "aws_security_group_rule" "webserver_sg_inbound_rule_1" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.alb_sg.id
+  security_group_id = aws_security_group.webserver_sg.id
+}
+
+resource "aws_security_group_rule" "webserver_sg_inbound_rule_2" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.alb_sg.id
+  security_group_id = aws_security_group.webserver_sg.id
+}
+
+resource "aws_security_group_rule" "webserver_sg_inbound_rule_3" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.ssh_sg.id
+  security_group_id = aws_security_group.webserver_sg.id
+}
+
+##################### Database Security Group #####################
+
+resource "aws_security_group" "db_sg" {
+  name        = "db_sg"
+  description = "Database Security Group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+}
+
+resource "aws_security_group_rule" "db_sg_inbound_rule_1" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.webserver_sg.id
+  security_group_id = aws_security_group.db_sg.id
+}
+
+
+##################### EFS Security Group #####################
+
+resource "aws_security_group" "efs_sg" {
+  name        = "efs_sg"
+  description = "EFS Security Group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+}
+
+resource "aws_security_group_rule" "efs_sg_inbound_rule_1" {
+  type              = "ingress"
+  from_port         = 2049
+  to_port           = 2049
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.webserver_sg.id
+  security_group_id = aws_security_group.efs_sg.id
+}
+
+resource "aws_security_group_rule" "efs_sg_inbound_rule_2" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.ssh_sg.id
+  security_group_id = aws_security_group.efs_sg.id
+}
+
+resource "aws_security_group_rule" "efs_sg_inbound_rule_3" {
+  type              = "ingress"
+  from_port         = 2049
+  to_port           = 2049
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.efs_sg.id
+  security_group_id = aws_security_group.efs_sg.id
+}
+
+
+##################### ACM #####################
+
+resource "aws_acm_certificate" "Pepperoni_Certificate" {
+  domain_name       = "pimlicofilms.com"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "Pepperoni_Certificate_Validation" {
+  certificate_arn = aws_acm_certificate.Pepperoni_Certificate.arn
+}
