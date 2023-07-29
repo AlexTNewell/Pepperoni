e4b668b069e214fedf69030676ac7c8e0b279d07
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-07-29 20:40:27 +0100

    Add files via upload

---
+++ WordPress Hosted on EC2/efs.tf
@@ -0,0 +1,19 @@
+##################### EFS #####################
+
+resource "aws_efs_file_system" "dev_efs" {
+  creation_token = "my_efs"
+}
+
+##################### EFS Mount Targets #####################
+
+resource "aws_efs_mount_target" "alpha" {
+  file_system_id = aws_efs_file_system.dev_efs.id
+  subnet_id      = aws_subnet.pri_db_az_1.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_efs_mount_target" "bravo" {
+  file_system_id = aws_efs_file_system.dev_efs.id
+  subnet_id      = aws_subnet.pri_db_az_2.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
