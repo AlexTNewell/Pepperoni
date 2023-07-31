bca556da4e9b9d622975915b5948e3dd38a2a1eb
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-07-31 17:44:32 +0100

    Add files via upload

---
+++ WordPress Hosted on EC2/outputs.tf
@@ -0,0 +1,12 @@
+##################### Output Setup Server EC2 Instance ID #####################
+
+output "instance_id" {
+  value = aws_instance.setup_server.id
+}
+
+##################### Output Setup Server Private Key #####################
+
+output "setup_server_private_key" {
+  value = tls_private_key.rsa.private_key_pem
+}
+
