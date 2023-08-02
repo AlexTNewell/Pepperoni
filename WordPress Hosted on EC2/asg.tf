bcf365b250a2a23eab7493cfe8eb92a449f183d4
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 11:21:01 +0100

    Update asg.tf

--- WordPress Hosted on EC2/asg.tf
+++ WordPress Hosted on EC2/asg.tf
@@ -4,13 +4,10 @@ resource "aws_launch_template" "dev_launch_template" {
   instance_type        = "t2.micro"
   key_name             = aws_key_pair.pepperoni_tf_key.key_name
   
-  
   provisioner "docker" {
     command = "run"
     image   = "anewellcloud/possible-solution:latest"
   }
-
-  user_data = "${base64encode(data.template_file.test.rendered)}"
 
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
   
@@ -21,6 +18,8 @@ resource "aws_launch_template" "dev_launch_template" {
   lifecycle {
     create_before_destroy = true
   }
+
+  user_data = "${base64encode(data.template_file.user_data.rendered)}"
 }
 
 ##################### ASG #####################
