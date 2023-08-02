d8ebb0204b11d50f7e5e3853e6e6d3036cef455e
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 16:34:08 +0100

    Update route.tf

--- WordPress Hosted on EC2/route.tf
+++ WordPress Hosted on EC2/route.tf
@@ -12,18 +12,27 @@ resource "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "validation_records" {
-  depends_on      = [aws_acm_certificate.Pepperoni_Certificate, aws_route53_zone.primary ]
+  depends_on      = [aws_acm_certificate.Pepperoni_Certificate]
   allow_overwrite = true
-  name   = "www"
-  records = ["thelondonchesssystem.com"]
-  type   = "CNAME"
+
+  for_each = {
+    for dvo in aws_acm_certificate.Pepperoni_Certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name   = each.value.name
+  records = [each.value.record]
+  type   = each.value.type
   zone_id = aws_route53_zone.primary.zone_id
   ttl             = 60
 }
 
 resource "aws_acm_certificate_validation" "Pepperoni_Certificate_Validation" {
   certificate_arn = aws_acm_certificate.Pepperoni_Certificate.arn
-  validation_record_fqdns = [ aws_route53_record.validation_records.fqdn]
+  validation_record_fqdns = [ aws_route53_record.*.fqdn]
 }
 
 resource "aws_route53_record" "www" {
