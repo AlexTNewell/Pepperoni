fabea1a15081343cd9be54ae89da2f1f6c942e98
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 13:19:37 +0100

    Update security.tf

--- WordPress Hosted on EC2/security.tf
+++ WordPress Hosted on EC2/security.tf
@@ -117,14 +117,3 @@ resource "aws_security_group_rule" "efs_sg_inbound_rule_3" {
   security_group_id = aws_security_group.efs_sg.id
 }
 
-
-##################### ACM #####################
-
-resource "aws_acm_certificate" "Pepperoni_Certificate" {
-  domain_name       = "thelondonchesssystem.com"
-  validation_method = "DNS"
-}
-
-resource "aws_acm_certificate_validation" "Pepperoni_Certificate_Validation" {
-  certificate_arn = aws_acm_certificate.Pepperoni_Certificate.arn
-}
