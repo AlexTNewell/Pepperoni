da8771bb4f678afe83d997d4752344099df8a5ec
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 13:21:59 +0100

    Update route.tf

--- WordPress Hosted on EC2/route.tf
+++ WordPress Hosted on EC2/route.tf
@@ -12,4 +12,34 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_zone" "primary" {
   name = "thelondonchesssystem.com"
+  private_zone = false
+}
+
+##################### ACM #####################
+
+resource "aws_acm_certificate" "Pepperoni_Certificate" {
+  domain_name       = "thelondonchesssystem.com"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "Pepperoni_Certificate_Validation" {
+  certificate_arn = aws_acm_certificate.Pepperoni_Certificate.arn
+}
+
+
+resource "aws_route53_record" "example" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
 }
