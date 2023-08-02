21b372b2df2804a876dd5f1178c5d54fc562940d
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 14:32:05 +0100

    Update route.tf

--- WordPress Hosted on EC2/route.tf
+++ WordPress Hosted on EC2/route.tf
@@ -8,29 +8,20 @@ resource "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "validation_records" {
-  for_each = {
-    for dvo in aws_acm_certificate.Pepperoni_Certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
+  name   = tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_name
+  record = [ tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_value ]
+  type   = tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_type
+  zone_id = aws_route53_zone.primary.zone_id
   ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.primary.zone_id
 }
 
 resource "aws_acm_certificate_validation" "Pepperoni_Certificate_Validation" {
   certificate_arn = aws_acm_certificate.Pepperoni_Certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
+  validation_record_fqdns = [ aws_route53_record.validation_records.fqdn]
 }
 
 resource "aws_route53_record" "www" {
-
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.thelondonchesssystem.com"
   type    = "A"
@@ -38,6 +29,6 @@ resource "aws_route53_record" "www" {
   alias {
       name                   = aws_lb.app_alb.dns_name
       zone_id                = aws_lb.app_alb.zone_id
-      evaluate_target_health = true
+      evaluate_target_health = false
     }
 }
