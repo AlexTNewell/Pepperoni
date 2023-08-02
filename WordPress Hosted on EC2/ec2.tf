72cef1c3d4758954b0b18443f97fe9801a341afc
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 10:40:44 +0100

    Update ec2.tf

--- WordPress Hosted on EC2/ec2.tf
+++ WordPress Hosted on EC2/ec2.tf
@@ -51,21 +51,18 @@ resource "aws_instance" "app_server_az1" {
 
   user_data = <<-EOT
 #!/bin/bash
-sudo su
 yum update -y
-sudo apt update
-sudo apt install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker $(whoami)
-docker --version
-sudo git clone https://github.com/AlexTNewell/possible-solution.git
-sudo yum install -y amazon-efs-utils
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+git clone https://github.com/AlexTNewell/possible-solution.git
+yum install -y amazon-efs-utils  # For Amazon Linux
 mkdir -p /var/www/docker_resources
-sudo echo "data.aws_efs_file_system.dev_efs.dns_name:/ /var/www/docker_resources nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+echo "data.aws_efs_file_system.dev_efs.dns_name:/ /var/www/docker_resources nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
 mount -a
-sudo cp -r /home/ec2-user/possible-solution/* /var/www/docker_resources
-sudo docker run -d -p 80:80 -v /home/ec2-user/possible-solution/main/sub:/www anewellcloud/possible-solution:latest
+cp -r /home/ec2-user/possible-solution/* /var/www/docker_resources
+docker run -d -p 80:80 -v /var/www/docker_resources/main/sub:/www anewellcloud/possible-solution:latest
 EOT
 }
 
@@ -89,21 +86,18 @@ resource "aws_instance" "app_server_az2" {
   }
   user_data = <<-EOT
 #!/bin/bash
-sudo su
 yum update -y
-sudo apt update
-sudo apt install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker $(whoami)
-docker --version
-sudo git clone https://github.com/AlexTNewell/possible-solution.git
-sudo yum install -y amazon-efs-utils
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+git clone https://github.com/AlexTNewell/possible-solution.git
+yum install -y amazon-efs-utils  # For Amazon Linux
 mkdir -p /var/www/docker_resources
-sudo echo "data.aws_efs_file_system.dev_efs.dns_name:/ /var/www/docker_resources nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+echo "data.aws_efs_file_system.dev_efs.dns_name:/ /var/www/docker_resources nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
 mount -a
-sudo cp -r /home/ec2-user/possible-solution/* /var/www/docker_resources
-sudo docker run -d -p 80:80 -v /home/ec2-user/possible-solution/main/sub:/www anewellcloud/possible-solution:latest
+cp -r /home/ec2-user/possible-solution/* /var/www/docker_resources
+docker run -d -p 80:80 -v /var/www/docker_resources/main/sub:/www anewellcloud/possible-solution:latest
 EOT
 }
 
