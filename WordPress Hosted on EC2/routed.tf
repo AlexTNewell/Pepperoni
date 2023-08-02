f71809bdb287009e8e50ad1f27c2c45698c70958
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 12:42:02 +0100

    Update routed.tf

--- WordPress Hosted on EC2/routed.tf
+++ WordPress Hosted on EC2/routed.tf
@@ -1,7 +1,15 @@
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
+  name    = "www.thelondonchesssystem.com"
   type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
+
+  alias {
+      name                   = aws_lb.app_alb.dns_name
+      zone_id                = aws_lb.app_alb.zone_id
+      evaluate_target_health = true
+    }
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "thelondonchesssystem.com"
 }
