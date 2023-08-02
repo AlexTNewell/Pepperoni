d55e0865dc1ac8e4fbfe28dcf3779d7521ead381
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-02 14:36:43 +0100

    Update route.tf

--- WordPress Hosted on EC2/route.tf
+++ WordPress Hosted on EC2/route.tf
@@ -10,7 +10,7 @@ resource "aws_route53_zone" "primary" {
 resource "aws_route53_record" "validation_records" {
   allow_overwrite = true
   name   = tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_name
-  record = [ tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_value ]
+  records = [ tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_value ]
   type   = tolist(aws_acm_certificate.Pepperoni_Certificate.domain_validation_options)[0].resource_record_type
   zone_id = aws_route53_zone.primary.zone_id
   ttl             = 60
