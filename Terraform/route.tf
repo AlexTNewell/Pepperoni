cdff8c97e16cb149ccdcb8fb876ce199fdcbeb40
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-03 09:49:43 +0100

    Update route.tf

--- Terraform/route.tf
+++ Terraform/route.tf
@@ -12,27 +12,22 @@ resource "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "validation_records" {
-  depends_on      = [aws_acm_certificate.Pepperoni_Certificate]
+  depends_on      = [aws_acm_certificate.Pepperoni_Certificate, aws_route53_zone.primary]
   allow_overwrite = true
 
-  for_each = {
-    for dvo in aws_acm_certificate.Pepperoni_Certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  zone_id = data.aws_route53_zone.primary.zone_id
+  ttl = 60
 
-  name   = each.value.name
-  records = [each.value.record]
-  type   = each.value.type
-  zone_id = aws_route53_zone.primary.zone_id
-  ttl             = 60
+  name    = aws_acm_certificate.Pepperoni_Certificate.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.Pepperoni_Certificate.domain_validation_options.0.resource_record_type
+  records = [ aws_acm_certificate.Pepperoni_Certificate.domain_validation_options.0.resource_record_value ]
+
 }
 
 resource "aws_acm_certificate_validation" "Pepperoni_Certificate_Validation" {
+  depends_on      = [aws_acm_certificate.Pepperoni_Certificate, aws_route53_zone.primary]
   certificate_arn = aws_acm_certificate.Pepperoni_Certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
+  validation_record_fqdns = aws_acm_certificate.Pepperoni_Certificate.domain_validation_options.0.resource_record_name
 }
 
 resource "aws_route53_record" "www" {
